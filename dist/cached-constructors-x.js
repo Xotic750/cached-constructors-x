39628cdcfcb7f6409534b5e7445387ee43246469
Graham Fairweather <xotic750@gmail.com>
2019-07-31 23:39:28 +0200

    :bookmark: v2.0.25
    Auto build

--- dist/cached-constructors-x.js
+++ dist/cached-constructors-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-31T19:10:13.350Z",
+  "date": "2019-07-31T21:39:23.823Z",
   "describe": "",
   "description": "Constructors cached from literals.",
   "file": "cached-constructors-x.js",
   "hash": "6d282f9108b134ba6b0b",
   "license": "MIT",
-  "version": "2.0.24"
+  "version": "2.0.25"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
