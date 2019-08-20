f2b90f122b8d875b45d585b89dc0a4261689d54c
Graham Fairweather <xotic750@gmail.com>
2019-08-20 14:27:50 +0200

    Maintenance

--- dist/cached-constructors-x.js
+++ dist/cached-constructors-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-20T08:07:43.203Z",
+  "date": "2019-08-20T12:27:45.302Z",
   "describe": "",
   "description": "Constructors cached from literals.",
   "file": "cached-constructors-x.js",
